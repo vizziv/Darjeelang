f2803256617ee7aa6043112407c086c662fc92fa
Ziv Scully <zivscully@gmail.com>
2015-01-23 13:15:09 -0500

    First working example.

---
+++ darjeelang.hs
@@ -0,0 +1,154 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Darjeelang where
+
+import Data.Function
+import Data.List
+import Data.Maybe
+
+type TagName = String
+type VarName = String
+
+data Typ = TPrim
+         | TTag TagName Typ
+         | TFun [Typ] Typ
+         deriving (Eq, Show)
+
+data Op = Plus | Minus | Times deriving Show
+
+data ExpF e = EPrim Integer
+            | EOp Op e e
+            | ETag TagName e
+            | EUntag e
+            | EVar VarName
+            | EFun [(Typ, VarName)] e
+            | EApp e e
+            deriving Show
+
+newtype Exp = E (ExpF Exp) deriving Show
+
+data TypedExp = TE Typ (ExpF TypedExp) deriving Show
+
+{-
+For now, we handle things nicely only if the following hold.
+ * Argument type lists always appear in the same order.
+ * Lists of single values have no duplicates.
+ * Lists of tuples have no duplicate first members.
+ * Variable names are unique.
+We can handle the following errors but not gracefully.
+ * Expressions are not well-typed.
+ * Variables are not bound when referenced.
+-}
+
+type CheckContext = [(VarName, Typ)]
+
+fromCtx ctx var = fromJust $ lookup var ctx
+
+addArgsToCtx ctx args = (map (\(x,y) -> (y,x)) args) ++ ctx
+
+argsTyps = map fst
+
+checkUseArg typs typ =
+    case find (==typ) typs of
+      Nothing -> undefined
+      Just _ -> delete typ typs
+
+check :: CheckContext -> Exp -> TypedExp
+check ctx (E e) =
+    case e of
+      EPrim prim -> TE TPrim (EPrim prim)
+      EOp op exp1 exp2 -> let texp1@(TE TPrim _) = check ctx exp1
+                              texp2@(TE TPrim _) = check ctx exp2
+                          in TE TPrim (EOp op texp1 texp2)
+      ETag name exp -> let texp@(TE typ _) = check ctx exp
+                      in TE (TTag name typ) (ETag name texp)
+      EUntag exp -> let texp@(TE (TTag _ typ) _) = check ctx exp
+                    in TE typ (EUntag texp)
+      EVar var -> TE (fromCtx ctx var) (EVar var)
+      EFun args exp -> let texp@(TE typ _) = check (addArgsToCtx ctx args) exp
+                       in TE (TFun (argsTyps args) typ) (EFun args texp)
+      EApp exp1 exp2 -> let texp1@(TE (TFun typsA typZ) _) = check ctx exp1
+                            texp2@(TE typ2 _) = check ctx exp2
+                            typsB = checkUseArg typsA typ2
+                            typ = case typsB of
+                                    [] -> typZ
+                                    _ -> TFun typsB typZ
+                        in TE typ (EApp texp1 texp2)
+
+applyOp Plus = (+)
+applyOp Minus = (-)
+applyOp Times = (*)
+
+data Result = RPrim Integer
+            | RTag TagName Result
+            | RFun EvalContext [(Typ, VarName)] TypedExp
+            deriving Show
+
+type EvalContext = [(VarName, Result)]
+
+evalUseArg args (TE typ exp) =
+    case lookup typ args of
+      Nothing -> undefined
+      Just var -> (var, delete (typ, var) args)
+
+-- Assumes expressions are well-typed, e.g. EOps are applied only to EPrims.
+eval :: EvalContext -> TypedExp -> Result
+eval ctx (TE t e) =
+    case e of
+      EPrim n -> RPrim n
+      EOp op texp1 texp2 -> let RPrim n1 = eval ctx texp1
+                                RPrim n2 = eval ctx texp2
+                            in RPrim (applyOp op n1 n2)
+      ETag name texp -> RTag name (eval ctx texp)
+      EUntag texp -> let RTag _ result = eval ctx texp in result
+      EVar var -> fromCtx ctx var
+      EFun args texp -> RFun ctx args texp
+      EApp texp1 texp2 -> let RFun ctx1 args1 texp1' = eval ctx texp1
+                              (var, args') = evalUseArg args1 texp2
+                              ctx1' = (var, eval ctx texp2) : ctx1
+                          in case args' of
+                               [] -> eval ctx1' texp1'
+                               _ -> RFun ctx1' args' texp1'
+
+expSubtract = E $ EFun [(TTag "fst" TPrim, "x"), (TTag "snd" TPrim, "y")] $
+                       E $ EOp Minus (E $ EUntag $ E $ EVar "x")
+                                     (E $ EUntag $ E $ EVar "y")
+
+exp5 = E $ EApp (E $ EApp expSubtract (E $ ETag "snd" $ E $ EPrim 4))
+                                      (E $ ETag "fst" $ E $ EPrim 9)
+
+class Expable a where
+    inj :: a -> Exp
+
+instance Expable Exp where
+    inj = id
+
+instance Integral a => Expable [[a]] where
+    inj = E . EPrim . fromIntegral . head . head
+
+instance Expable [Char] where
+    inj = E . EVar
+
+e1 con = E . con . inj
+e2 con x y = E $ con (inj x) (inj y)
+
+(+~), (-~), (*~) :: (Expable a, Expable b) => a -> b -> Exp
+(+~) = e2 $ EOp Plus
+(-~) = e2 $ EOp Minus
+(*~) = e2 $ EOp Times
+
+tag :: Expable a => TagName -> a -> Exp
+tag name = e1 $ ETag name
+
+untag :: Expable a => a -> Exp
+untag = e1 EUntag
+
+fun :: Expable a => [(Typ, VarName)] -> a -> Exp
+fun args = e1 $ EFun args
+
+(%) :: (Expable a, Expable b) => a -> b -> Exp
+(%) = e2 EApp
+
+expSubtract' = fun [(TTag "fst" TPrim, "x"), (TTag "snd" TPrim, "y")] $ untag "x" -~ untag "y"
+exp5' :: Exp
+exp5' = expSubtract' % tag "snd" [[4]] % tag "fst" [[9]]
