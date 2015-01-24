ce08377f401a1317363480e9b3a759ed465ab13f
Ziv Scully <zivscully@gmail.com>
2015-01-23 22:18:00 -0500

    Begin adding let bindings.

--- darjeelang.hs
+++ darjeelang.hs
@@ -44,7 +44,8 @@ type CheckContext = [(VarName, Typ)]
 
 fromCtx ctx var = fromJust $ lookup var ctx
 
-addArgsToCtx ctx args = (map (\(x,y) -> (y,x)) args) ++ ctx
+addArgsToCtx ctx args = map (\(typ,var) -> (var,typ)) args ++ ctx
+addDeclsToCtx ctx decls = map (\(typ,var,_) -> (var,typ)) decls ++ ctx
 
 argsTyps = map fst
 
@@ -55,32 +56,46 @@ checkUseArg typs typ =
 
 check :: CheckContext -> Exp -> TypedExp
 check ctx (E e) =
-    let ch = check ctx in
-    case e of
-      EPrim prim -> TE TPrim (EPrim prim)
-      EOp op exp1 exp2 -> let texp1@(TE TPrim _) = ch exp1
-                              texp2@(TE TPrim _) = ch exp2
-                          in TE TPrim (EOp op texp1 texp2)
-      ETag name exp -> let texp@(TE typ _) = ch exp
-                      in TE (TTag name typ) (ETag name texp)
-      EUntag exp -> let texp@(TE (TTag _ typ) _) = ch exp
-                    in  TE typ (EUntag texp)
-      EVar var -> TE (fromCtx ctx var) (EVar var)
-      EFun args exp -> let texp@(TE typ _) = check (addArgsToCtx ctx args) exp
-                       in TE (TFun (argsTyps args) typ) (EFun args texp)
-      EApp exp1 exp2 -> let texp1@(TE (TFun typsA typZ) _) = ch exp1
-                            texp2@(TE typ2 _) = ch exp2
-                            typsB = checkUseArg typsA typ2
-                            typ = case typsB of
-                                    [] -> typZ
-                                    _ -> TFun typsB typZ
-                        in TE typ (EApp texp1 texp2)
-      EBranch exp1 exp2 exp3 -> let texp1@(TE TPrim _) = ch exp1
-                                    texp2@(TE typ2 _) = ch exp2
-                                    texp3@(TE typ3 _) = ch exp3
-                                in if typ2 == typ3
-                                   then TE typ2 (EBranch texp1 texp2 texp3)
-                                   else undefined
+    let ch = check ctx
+    in case e of
+         EPrim prim -> TE TPrim (EPrim prim)
+         EOp op exp1 exp2 ->
+             let texp1@(TE TPrim _) = ch exp1
+                 texp2@(TE TPrim _) = ch exp2
+             in TE TPrim (EOp op texp1 texp2)
+         ETag name exp ->
+             let texp@(TE typ _) = ch exp
+             in TE (TTag name typ) (ETag name texp)
+         EUntag exp ->
+             let texp@(TE (TTag _ typ) _) = ch exp
+             in TE typ (EUntag texp)
+         EVar var -> TE (fromCtx ctx var) (EVar var)
+         EFun args exp ->
+             let texp@(TE typ _) = check (addArgsToCtx ctx args) exp
+             in TE (TFun (argsTyps args) typ) (EFun args texp)
+         EApp exp1 exp2 ->
+             let texp1@(TE (TFun typsA typZ) _) = ch exp1
+                 texp2@(TE typ2 _) = ch exp2
+                 typsB = checkUseArg typsA typ2
+                 typ = case typsB of
+                         [] -> typZ
+                         _ -> TFun typsB typZ
+             in TE typ (EApp texp1 texp2)
+         EBranch exp1 exp2 exp3 ->
+             let texp1@(TE TPrim _) = ch exp1
+                 texp2@(TE typ2 _) = ch exp2
+                 texp3@(TE typ3 _) = ch exp3
+             in if typ2 == typ3
+                then TE typ2 (EBranch texp1 texp2 texp3)
+                else undefined
+         ELet decls exp ->
+             let ch' = check (addDeclsToCtx ctx decls)
+                 chDecl (typ,var,exp') = let texp'@(TE typ' _) = ch' exp'
+                                         in if typ == typ'
+                                            then (typ,var,texp')
+                                            else undefined
+                 texp@(TE typ _) = ch' exp
+             in TE typ (ELet (map chDecl decls) texp)
 
 data Result = RPrim Integer
             | RTag TagName Result
@@ -101,24 +116,27 @@ evalUseArg args (TE typ exp) =
 -- Assumes expressions are well-typed, e.g. EOps are applied only to EPrims.
 eval :: EvalContext -> TypedExp -> Result
 eval ctx (TE t e) =
-    let ev = eval ctx in
-    case e of
-      EPrim n -> RPrim n
-      EOp op texp1 texp2 -> let RPrim n1 = ev texp1
-                                RPrim n2 = ev texp2
-                            in RPrim (applyOp op n1 n2)
-      ETag name texp -> RTag name (ev texp)
-      EUntag texp -> let RTag _ result = ev texp in result
-      EVar var -> fromCtx ctx var
-      EFun args texp -> RFun ctx args texp
-      EApp texp1 texp2 -> let RFun ctx1 args1 texp1' = ev texp1
-                              (var, args') = evalUseArg args1 texp2
-                              ctx1' = (var, ev texp2) : ctx1
-                          in case args' of
-                               [] -> eval ctx1' texp1'
-                               _ -> RFun ctx1' args' texp1'
-      EBranch texp1 texp2 texp3 -> let RPrim n1 = ev texp1
-                                   in ev (if n1 /= 0 then texp2 else texp3)
+    let ev = eval ctx
+    in case e of
+         EPrim n -> RPrim n
+         EOp op texp1 texp2 ->
+             let RPrim n1 = ev texp1
+                 RPrim n2 = ev texp2
+             in RPrim (applyOp op n1 n2)
+         ETag name texp -> RTag name (ev texp)
+         EUntag texp -> let RTag _ result = ev texp in result
+         EVar var -> fromCtx ctx var
+         EFun args texp -> RFun ctx args texp
+         EApp texp1 texp2 ->
+             let RFun ctx1 args1 texp1' = ev texp1
+                 (var, args') = evalUseArg args1 texp2
+                 ctx1' = (var, ev texp2) : ctx1
+             in case args' of
+                  [] -> eval ctx1' texp1'
+                  _ -> RFun ctx1' args' texp1'
+         EBranch texp1 texp2 texp3 ->
+             let RPrim n1 = ev texp1
+             in ev (if n1 /= 0 then texp2 else texp3)
 
 run = eval [] . check []
 
