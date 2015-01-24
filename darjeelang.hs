29e6d591e11e9a1e6cb094b435b44890498e4d2a
Ziv Scully <zivscully@gmail.com>
2015-01-24 00:05:48 -0500

    Implement let bindings and test them in factorial and Fibonacci examples.

--- darjeelang.hs
+++ darjeelang.hs
@@ -22,7 +22,7 @@ data ExpF e = EPrim Integer
             | EFun [(Typ, VarName)] e
             | EApp e e
             | EBranch e e e
-            | ELet [(Typ, VarName, e)] e
+            | ELets [(Typ, VarName, e)] e
             deriving Show
 
 newtype Exp = E (ExpF Exp) deriving Show
@@ -88,14 +88,14 @@ check ctx (E e) =
              in if typ2 == typ3
                 then TE typ2 (EBranch texp1 texp2 texp3)
                 else undefined
-         ELet decls exp ->
+         ELets decls exp ->
              let ch' = check (addDeclsToCtx ctx decls)
                  chDecl (typ,var,exp') = let texp'@(TE typ' _) = ch' exp'
                                          in if typ == typ'
                                             then (typ,var,texp')
                                             else undefined
                  texp@(TE typ _) = ch' exp
-             in TE typ (ELet (map chDecl decls) texp)
+             in TE typ (ELets (map chDecl decls) texp)
 
 data Result = RPrim Integer
             | RTag TagName Result
@@ -136,21 +136,29 @@ eval ctx (TE t e) =
                   _ -> RFun ctx1' args' texp1'
          EBranch texp1 texp2 texp3 ->
              let RPrim n1 = ev texp1
-             in ev (if n1 /= 0 then texp2 else texp3)
+             in ev (if n1 == 0 then texp2 else texp3)
+         ELets decls texp ->
+             let ctx' = map (\(_,var,texp') -> (var, eval ctx' texp')) decls ++ ctx
+             in eval ctx' texp
 
 run = eval [] . check []
 
 e2 con x y = E $ con x y
 prim = E . EPrim
+infixl 6 +~
 (+~) = e2 $ EOp Plus
+infixl 6 -~
 (-~) = e2 $ EOp Minus
+infixl 6 *~
 (*~) = e2 $ EOp Times
 var = E . EVar
 tag = e2 ETag
 untag = E . EUntag
 fun = e2 EFun
+infixl 8 %
 (%) = e2 EApp
 branch exp1 exp2 exp3 = E $ EBranch exp1 exp2 exp3
+lets = e2 ELets
 
 sub = fun [(TTag "fst" TPrim, "x"), (TTag "snd" TPrim, "y")]
           (untag (var "x") -~ untag (var "y"))
@@ -159,3 +167,20 @@ minus5 = fun [(TPrim, "x")] (var "x" -~ prim 5)
 pick = fun [(TTag "choice" TPrim, "b"), (TPrim, "x")]
            ((branch (untag (var "b")) plus3 minus5) % var "x")
 app = fun [(TFun [TPrim] TPrim, "f"), (TPrim, "x")] (var "f" % var "x")
+factorial = lets [(TFun [TPrim] TPrim, "f",
+                   fun [(TPrim, "x")]
+                       (branch (var "x")
+                               (prim 1)
+                               (var "x" *~ var "f" % (var "x" -~ prim 1))))]
+                 (var "f")
+fibonacci = lets [(TFun [TTag "0" TPrim, TTag "1" TPrim, TPrim] TPrim, "f",
+                   fun [(TTag "0" TPrim, "y"),
+                        (TTag "1" TPrim, "z"),
+                        (TPrim, "x")]
+                        (branch (var "x")
+                                (untag (var "y"))
+                               (var "f" % tag "0" (untag (var "z")) %
+                                          tag "1" (untag (var "y") +~
+                                                   untag (var "z")) %
+                                          (var "x" -~ prim 1))))]
+                 (var "f" % tag "0" (prim 0) % tag "1" (prim 1))
